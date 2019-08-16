1b6f87e3bf94440bdddf5ef3c0842279edb1e746
Ssskorobogatov <52072789+Ssskorobogatov@users.noreply.github.com>
2019-08-16 09:11:17 +0400

    Create MyException.java

---
+++ src/com/epam/Sergei_Skorobogatov/java/lesson3/task3/MyException.java
@@ -0,0 +1,8 @@
+package com.epam.Sergei_Skorobogatov.java.module_2;
+
+public class MyException extends Exception {
+    public MyException(String description) {
+        super(description);
+    }
+
+}
