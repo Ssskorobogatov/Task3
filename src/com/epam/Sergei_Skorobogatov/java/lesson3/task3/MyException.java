00a5aeb1284c35e233aa8f6e074334c0ef626a15
Sergei Skorobogatov <sergei_skorobogatov@epam.com>
2019-08-16 10:38:57 +0400

    update

--- src/com/epam/Sergei_Skorobogatov/java/lesson3/task3/MyException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.Sergei_Skorobogatov.java.module_2;
-
-public class MyException extends Exception {
-    public MyException(String description) {
-        super(description);
-    }
-
-}
